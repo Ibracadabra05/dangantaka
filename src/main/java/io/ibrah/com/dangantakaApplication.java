4f80a314b1727afb73e06a2cb223e74bcca5e9b0
Ibrahim Hamisu <ibrahim@sproutsocial.com>
2019-04-29 16:15:18 -0500

    first commit with skeleton code

---
+++ src/main/java/io/ibrah/com/dangantakaApplication.java
@@ -0,0 +1,29 @@
+package io.ibrah.com;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class dangantakaApplication extends Application<dangantakaConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new dangantakaApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "dangantaka";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<dangantakaConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final dangantakaConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/io/ibrah/com/dangantakaConfiguration.java
@@ -0,0 +1,10 @@
+package io.ibrah.com;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class dangantakaConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
