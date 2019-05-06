bf9d53c78f8d41f901f023347c93a80ba1443623
Ibrahim Hamisu <ibrahim@sproutsocial.com>
2019-05-06 13:49:25 -0500

    Query all users

--- src/main/java/io/ibrah/com/dangantakaApplication.java
+++ src/main/java/io/ibrah/com/dangantakaApplication.java
@@ -5,6 +5,10 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.ibrah.com.resources.UserResource;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class dangantakaApplication extends Application<dangantakaConfiguration> {
 
     public static void main(final String[] args) throws Exception {
@@ -24,7 +28,28 @@ public class dangantakaApplication extends Application<dangantakaConfiguration> 
     @Override
     public void run(final dangantakaConfiguration configuration,
                     final Environment environment) {
-        environment.jersey().register(new UserResource());
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/website?user=Ibrahim&password=users");
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        environment.jersey().register(new UserResource(conn));
+
     }
 
 }
--- src/main/java/io/ibrah/com/resources/UserResource.java
+++ src/main/java/io/ibrah/com/resources/UserResource.java
@@ -7,6 +7,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 
 /**
  * Created by ibrahimhamisu on 4/30/19.
@@ -15,9 +20,44 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
+    private Connection userConnection;
+
+    public UserResource(Connection userConnection) {
+        this.userConnection = userConnection;
+    }
+
     @GET
     @Path("{id}")
     public User getUser(final @PathParam("id") Long userId) {
         return new User();
+    }
+
+    @GET
+    @Path("{id}/followers/")
+    public User[] getFollowers(final @PathParam("id") Long userId) {
+        return  new User[5];
+    }
+
+    @GET
+    @Path("{id}/follows/")
+    public User[] getFollows(final @PathParam("id") Long userId) {
+        return new User[3];
+    }
+
+    @GET
+    public User[] getAllUsers(){
+        try {
+            Statement stmt = this.userConnection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " : " + rs.getString(2));
+            }
+            this.userConnection.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getStackTrace().toString());
+        }
+        return new User[10];
     }
 }
